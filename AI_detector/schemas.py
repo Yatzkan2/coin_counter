c402b0c5b50795abdd92b437115cf6d4f114dd87
Yair Yatzkan <yairyatzkan@Yairs-MacBook-Pro.local>
2024-12-12 19:18:45 +0200

    added pydantic schemas

---
+++ AI_detector/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import List, Dict
+
+
+# Response model for individual predictions
+class Prediction(BaseModel):
+    class_id: int
+    class_name: str
+    confidence: float
+
+# Response model for the API
+class PredictionResponse(BaseModel):
+    predictions: List[Prediction]
